8c83d3d1b0d0c8766e609ba9ad702c01109609ad
esbendg <esdrga@ruc.dk>
2022-11-22 14:30:17 +0100

    It works

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Matrixgraph thisgraph = new Matrixgraph(7);
-        int [] differenceGraph = new int[7];
-        int biggest = 0;
+        Matrixgraph thisgraph = new Matrixgraph(7); //First graph, instantiated with numbers from the assignment
+        Matrixgraph newgraph = new Matrixgraph(7); //second graph, calculated with the algorithm.
+        int [] differenceGraph = new int[7]; //list made to save the difference in outgoing and incoming containers
+        int moneyUsed = 0;
+
         thisgraph.addEdge(0,3,2000);
         thisgraph.addEdge(0,2,2000);
         thisgraph.addEdge(1,3,5000);
@@ -17,8 +19,9 @@ public class Main {
         thisgraph.addEdge(3,6,2000);
         thisgraph.addEdge(3,5,500);
 
-        thisgraph.printGraph();
-        System.out.println("thisgraph printed");
+        System.out.println("Containers going out using these vertices: ");
+        thisgraph.printGraph(); //Method to print the graph
+
         /*
         Calculating the difference, by checking if there is an edge.
         If there is it subtracts outgoing containers, and adds incoming containers.
@@ -31,14 +34,30 @@ public class Main {
                 }
             }
         }
+        /*
+        Finding the highest and lowest digits in the difference list, and their respective indexes.
+         */
+        int amountatlow = differenceGraph[thisgraph.findLowestindex(differenceGraph)];
+        int amountathigh = differenceGraph[thisgraph.findBiggestindex(differenceGraph)];
 
-        for(int i =0;i<differenceGraph.length; i++){
-            System.out.println(differenceGraph[i]);
+        /*
+        Using a while loop to calculate until the amount at low difference in the list is 0.
+        Adding vertices to a new graph, and calculating the difference so it changes in the difference list.
+        getting a new highest and lowest number from the difference list and continuing.
+         */
+
+        while (amountatlow!=0) {
+            newgraph.addEdge(thisgraph.findBiggestindex(differenceGraph),thisgraph.findLowestindex(differenceGraph), amountathigh);
+            differenceGraph[thisgraph.findLowestindex(differenceGraph)] = amountatlow + amountathigh;
+            differenceGraph[thisgraph.findBiggestindex(differenceGraph)] = 0;
+
+            amountatlow = differenceGraph[thisgraph.findLowestindex(differenceGraph)];
+            amountathigh = differenceGraph[thisgraph.findBiggestindex(differenceGraph)];
+            moneyUsed = moneyUsed + amountathigh;
         }
 
-
-        System.out.println("highest: "+ thisgraph.findBiggestindex(differenceGraph));
-        System.out.println("Lowest: "+ thisgraph.findLowestindex(differenceGraph));
+        System.out.println("Containers return to their origin at minimum cost: ");
+        newgraph.printGraph();
+        System.out.println("Total Costs using this algorithm: " + moneyUsed);
     }
-
 }
